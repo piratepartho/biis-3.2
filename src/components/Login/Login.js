9103ee058018392614e17b7e209d6263c173486b
piratepartho <76593389+piratepartho@users.noreply.github.com>
2023-02-28 19:36:17 +0600

    loginstyle done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,32 +2,17 @@ import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import FormControl, TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
+import { TextField } from '@mui/material';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const theme = createTheme();
-import { useNavigate } from "react-router-dom";
+
 
 const Login = (props) => {
     const [username, setUserName] = useState("");
@@ -43,7 +28,8 @@ const Login = (props) => {
         setPassword(event.target.value);
     }
 
-    const loginHandler = () => {
+    const loginHandler = (e) => {
+        e.preventDefault();
         if(username === "e" && password === "1"){
             navigate("/student/");
         } else alert("login error");
@@ -52,7 +38,7 @@ const Login = (props) => {
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
+        <CssBaseline /> 
         <Box
           sx={{
             marginTop: 8,
